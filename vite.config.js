b1fdc99fd3f9eed94c42668950aa27a06237f885
lxcamargo <alex.camargogus@gmail.com>
2025-08-21 14:24:49 -0300

    Atualizações no sistema de estoque

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // garante que a raiz é o diretório atual
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html'),
-    },
   },
-});
+})
