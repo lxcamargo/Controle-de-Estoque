8daf80786abbdca079c45e87e9d3352e97ca512d
lxcamargo <alex.camargogus@gmail.com>
2025-08-22 11:30:49 -0300

    Remove src/vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  optimizeDeps: {
+    include: ['xlsx'],  // adiciona aqui para o Vite pré-bundlar o xlsx
+  },
   build: {
     outDir: 'dist',
   },
