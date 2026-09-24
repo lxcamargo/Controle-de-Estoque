8e5932f134a10b86e77139d8b390c19e56d5eed4
lxcamargo <168215929+lxcamargo@users.noreply.github.com>
2026-09-24 14:41:03 -0300

    Fix component name for saldo_galpao_loja route

--- src/App.jsx
+++ src/App.jsx
@@ -121,7 +121,7 @@ export default function App() {
         <Route path="/importar-estoque-loja" element={<ImportarEstoqueLoja />} />
         <Route path="/painel-validade-loja" element={<PainelValidadeLoja />} />
         <Route path="/historico-saidas-loja" element={<HistoricoSaidaLoja />} />
-        <Route path="/saldo_galpao_loja" element={<saldo_galpao_loja />} />
+        <Route path="/saldo_galpao_loja" element={<SaldoGalpaoLoja />} />
 
         {/* ✅ Nova rota para movimentações entre setores */}
         <Route path="/movimentacoes-galpao-loja" element={<MovimentacoesGalpaoLoja />} />
